aeff5625a342e9cccb435bf0a60b45bcef2154b5
Jiqing Yao <jiqingyao@gmail.com>
2025-02-20 13:54:02 -0500

    fix: cleanup the code

--- js/client.js
+++ js/client.js
@@ -1,61 +1,57 @@
 const COLOR_WAIT = "#42c2f4";
 const COLOR_OK = "#1f7f39";
 const COLOR_ERROR = "#c43131";
-const setMessage = function(message) {
-    consola.innerHTML = message;
+const setMessage = function (message) {
+  consola.innerHTML = message;
 };
 var active = false;
-const startListener = function() {
-    if (active)
-	return;
-    active = true;
-    wsConn = new WebSocket('ws://' + window.location.hostname + ":20001/");
+const startListener = function () {
+  if (active) return;
+  active = true;
+  wsConn = new WebSocket("ws://" + window.location.hostname + ":20001/");
 
-    wsConn.onopen = function() {
-	setMessage("Active", COLOR_OK);
+  wsConn.onopen = function () {
+    setMessage("Active", COLOR_OK);
+  };
+  wsConn.onclose = function (e) {
+    setMessage("Disconn. " + JSON.stringify(e), COLOR_ERROR);
+    active = false;
+  };
+  wsConn.onerror = function (e, code) {
+    setMessage("ERROR " + code + e, COLOR_ERROR);
+  };
+  wsConn.onmessage = function (message) {
+    try {
+      var datos = JSON.parse(message.data);
+      setMessage(datos.msj);
+      if (datos.cursor) {
+        var texto = "";
+        for (var i = 0; i < datos.cursor.y; i++) texto += "<br/>";
+        for (var i = 0; i < datos.cursor.x; i++) texto += "&nbsp;";
+        texto += "<span>_</span>";
+        cursor.innerHTML = texto;
+      }
+    } catch (e) {
+      setMessage("MSG ERROR", COLOR_ERROR);
     }
-    wsConn.onclose = function(e) {
-	setMessage("Disconn. " + JSON.stringify(e), COLOR_ERROR);
-	active = false;
-    }
-    wsConn.onerror = function(e, code) {
-	setMessage("ERROR " + code + e, COLOR_ERROR);
-    }
-    wsConn.onmessage = function(message) {
-	try {
-	    var datos = JSON.parse(message.data);
-	    setMessage(datos.msj);
-	    if (datos.cursor) {
-		var texto = "";
-		for (var i = 0; i < datos.cursor.y; i++)
-		    texto += "<br/>";
-		for (var i = 0; i < datos.cursor.x; i++)
-		    texto += "&nbsp;";
-		texto += "<span>_</span>";
-		cursor.innerHTML = texto;
-
-	    }
-	} catch(e) {
-	    setMessage("MSG ERROR", COLOR_ERROR);
-	}
-    }
-}
+  };
+};
 cursor.alt = false;
-setInterval(function() {
-    if (cursor.alt) {
-	cursor.className = "";
-    } else {
-	cursor.className = "alt";
-    }
-    cursor.alt = !cursor.alt;
+setInterval(function () {
+  if (cursor.alt) {
+    cursor.className = "";
+  } else {
+    cursor.className = "alt";
+  }
+  cursor.alt = !cursor.alt;
 }, 50);
 
-const rotarContenedor = function() {
-    contenedor.style.width = window.innerHeight + "px";
-    contenedor.style.height = window.innerWidth + "px";
-    contenedor.style.top = "-" + window.innerWidth + "px";
-    contenedor.className = "rotado";
-    startListener();
-}
+const rotarContenedor = function () {
+  contenedor.style.width = window.innerHeight + "px";
+  contenedor.style.height = window.innerWidth + "px";
+  contenedor.style.top = "-" + window.innerWidth + "px";
+  contenedor.className = "rotado";
+  startListener();
+};
 //contenedor.addEventListener("click", rotarContenedor);
 setTimeout(rotarContenedor, 500);
--- js/pvtable.js
+++ js/pvtable.js
@@ -1,44 +1,39 @@
-function GetPvLine(depth) {;
+function GetPvLine(depth) {
+  //console.log("GetPvLine");
 
-	//console.log("GetPvLine");
-	
-	var move = ProbePvTable();
-	var count = 0;
-	
-	while(move != NOMOVE && count < depth) {
-	
-		if( MoveExists(move) ) {
-			MakeMove(move);
-			brd_PvArray[count++] = move;
-			//console.log("GetPvLine added " + PrMove(move));	
-		} else {
-			break;
-		}		
-		move = ProbePvTable();	
-	}
-	
-	while(brd_ply > 0) {
-		TakeMove();
-	}
-	return count;
-	
+  var move = ProbePvTable();
+  var count = 0;
+
+  while (move != NOMOVE && count < depth) {
+    if (MoveExists(move)) {
+      MakeMove(move);
+      brd_PvArray[count++] = move;
+      //console.log("GetPvLine added " + PrMove(move));
+    } else {
+      break;
+    }
+    move = ProbePvTable();
+  }
+
+  while (brd_ply > 0) {
+    TakeMove();
+  }
+  return count;
 }
 
 function StorePvMove(move) {
+  var index = brd_posKey % PVENTRIES;
 
-	var index = brd_posKey % PVENTRIES;	
-	
-	brd_PvTable[index].move = move;
-    brd_PvTable[index].posKey = brd_posKey;
+  brd_PvTable[index].move = move;
+  brd_PvTable[index].posKey = brd_posKey;
 }
 
 function ProbePvTable() {
+  var index = brd_posKey % PVENTRIES;
 
-	var index = brd_posKey % PVENTRIES;	
-	
-	if( brd_PvTable[index].posKey == brd_posKey ) {
-		return brd_PvTable[index].move;
-	}
-	
-	return NOMOVE;
+  if (brd_PvTable[index].posKey == brd_posKey) {
+    return brd_PvTable[index].move;
+  }
+
+  return NOMOVE;
 }
